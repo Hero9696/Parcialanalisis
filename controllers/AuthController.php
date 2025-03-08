d76ee33e0d4ea6fcead738e6f2625762ce38dc00
Hero9696 <Miguelpadilla9619@gmail.com>
2025-03-08 15:13:01 -0600

    AuthControlle update

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -2,18 +2,36 @@
 session_start();
 require_once  __DIR__ . '/../views/modules/AuthModel.php';
 
+class TemplateController {
+
+    // Main view template
+    public function index() {
+        include __DIR__ . '/../views/template.php';
+    }
+
+    // Main route URL
+    static public function path() {
+        $protocol = (!empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") ? "https" : "http";
+        return $protocol . "://" . $_SERVER['HTTP_HOST'] . "/";
+    }
+}
+
+
 $authModel = new AuthModel();
-$error = "";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $user = $_POST['username'];
     $pass = $_POST['password'];
+
     if ($authModel->validateUser($user, $pass)) {
         $_SESSION['loggedin'] = true;
-        header("Location: ../views/pages/login/login.php");
-        exit();
+        $_SESSION['login_message'] = '<div class="alert alert-success">Inicio de sesión exitoso</div>';
     } else {
-        $error = "Credenciales incorrectas";
+        $_SESSION['login_message'] = '<div class="alert alert-danger">Credenciales incorrectas</div>';
     }
+
+    TemplateController::path();
+    header('Location: ' . TemplateController::path());
+    exit();
 }
 ?>
